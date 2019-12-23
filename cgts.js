c265f0f817b4a6b824b62548f30cd3a18b6b4f63
RwwL <rob@lifford.org>
2019-12-22 20:49:02 -0600

    refactor to use typical constructor function + prototype methods setup, update examples, tweak default options to current preferences

--- cgts.js
+++ cgts.js
@@ -1,129 +1,145 @@
-// namespace
-var cgts = cgts || {};
+function CircleGetsTheSquare(elem, options) {
+  var hasCanvas = !!document.createElement('canvas').getContext;
+  if (!hasCanvas) {
+    return new Error('CircleGetsTheSquare: requires the canvas element');
+  }
+  if (elem instanceof HTMLElement) {
+  	this.elem = elem;
+    this.resizeTimeout = null;
+    this.setOptions(options);
+    this.canvas = document.createElement('canvas');
+    this.canvas.classList.add(this.options.canvasClassName);
+    this.context =this.canvas.getContext('2d');
+    this.elem.prepend(this.canvas);
+    this.generate();
+    if(this.options.regenOnWinResize) {
+      var self = this;
+      window.addEventListener('resize', function(){
+        clearTimeout(self.resizeTimeout);
+        self.resizeTimeout = setTimeout( function() {
+          self.generate();
+        }, 200);
+      });
+    }
+  } else {
+    return new Error('CircleGetsTheSquare: invalid target element');
+  }
+};
 
-(function() {
+CircleGetsTheSquare.prototype.setOptions = function(options, regenerate) {
+  this.options = Object.assign({}, CircleGetsTheSquare.defaultOptions, this.options, options);
+  if (regenerate) {
+    this.generate();
+  }
+};
 
-	cgts.resizeTimeout = null;
-	cgts.resizeThrottle = 250;
-	cgts.defaultOptions = {
-		regenOnWinResize	: true,
-		canvasClassName	: 'cgtsCanvas',
-		bgColor			: 'white',
-		minRadius		: 30,
-		maxRadius		: 48,
-		minRectSide		: 60,
-		maxRectSide		: 150,
-		rMin			: 0,
-		rMax			: 255,
-		gMin			: 0,
-		gMax			: 255,
-		bMin			: 0,
-		bMax			: 255,
-		aMin			: 0.2,
-		aMax			: 1,
-		spacing			: 80,
-		jitter			: 8
-	};
-	cgts.randomColor = function(rMin, rMax, gMin, gMax, bMin, bMax, aMin, aMax) {
-		return 'rgba(' +
-			(Math.floor(Math.random()*rMax) + rMin) +
-			',' +
-			(Math.floor(Math.random()*gMax) + gMin) + 
-			',' + 
-			(Math.floor(Math.random()*bMax) + bMin) + 
-			',' + 
-			Math.floor(Math.random() * ((aMax*10) - (aMin*10)) + (aMin * 10)) / 10 +
-			')';
-	};
-	cgts.randomAngle = function() {
-		return Math.floor(Math.random()*360);
-	};
-	cgts.randomRadius = function(minRadius, maxRadius){
-		return (Math.floor(Math.random()*minRadius) + maxRadius);
-	},
-	cgts.jitterPoint =	function(point, jitter){
-		var jittered = point + cgts.posNeg(Math.ceil(Math.random()* jitter)-1);
-		return jittered;
-	}
-	cgts.posNeg = function(num) {
-		if (num == undefined) num = 1;
-		var gen = (Math.floor(Math.random()*5)) + 1;
-		return ((gen % 2 == 0) ? 1 : -1) * num;
-	},
-	cgts.drawCircle = function(ctx, jx, jy, r){
-		ctx.beginPath();
-		ctx.arc(jx, jy, r, 0, 2*Math.PI, true)
-		ctx.closePath();
-		ctx.fill();
-	},
-	cgts.drawRectangle = function(ctx, jx, jy, o) {
-		var
-			rectWidth = Math.floor(Math.random() * (o.maxRectSide - o.minRectSide)) + o.minRectSide,
-			rectHeight = Math.floor(Math.random() * (o.maxRectSide - o.minRectSide)) + o.minRectSide,
-			offsetX = -rectWidth/2,
-			offsetY = -rectHeight/2,
-			rotationInDegrees = cgts.randomAngle();
-			
-			ctx.translate(jx + rectWidth/2, jy + rectHeight/2);
-			ctx.rotate(Math.PI/180 * rotationInDegrees);
-			ctx.translate(offsetX, offsetY);   
-			ctx.fillRect(0, 0, rectWidth, rectHeight);
-					
-	}
+CircleGetsTheSquare.prototype.drawCircle = function(jx, jy, r){
+  var ctx = this.context;
+  ctx.beginPath();
+  ctx.arc(jx, jy, r, 0, 2 * Math.PI, true)
+  ctx.closePath();
+  ctx.fill();
+};
 
-	cgts.generate = function(elem, options) {
-		var hasCanvas = !!document.createElement('canvas').getContext;
-		if(!hasCanvas) return;
-		var
-			o = Object.assign({}, cgts.defaultOptions, options),
-			c,
-			ctx,
-			resizeTimeout,
-			resizeThrottle = o.resizeThrottle,
-			width	= elem.offsetWidth,
-			height = elem.offsetHeight;
-		if (o.bgColor) elem.style.backgroundColor = o.bgColor;
-		// test if an cgts canvas is there and reuse if it is
-		var firstChild = elem.firstChild
-		var hasPrevCanvas = firstChild && firstChild.nodeName.toLowerCase() === 'canvas' && firstChild.classList.contains(o.canvasClassName);
-		if ( hasPrevCanvas ) {
-				c	= firstChild;
-		}						
-		else {
-			var c	= document.createElement('canvas');
-			c.classList.add(o.canvasClassName);
-			elem.prepend(c);		
-		}
-	
-		var ctx = c.getContext('2d');			
-		c.width = width;
-		c.height = height;
-		ctx.clearRect(0, 0, width, height);	
-		for (var y = 0; y < height; y = y + o.spacing) {
-			for (var x = 0; x < width; x = x + o.spacing ) {
-				ctx.save();
-				ctx.fillStyle = cgts.randomColor(o.rMin, o.rMax, o.gMin, o.gMax, o.bMin, o.bMax, o.aMin, o.aMax);
-				var jx = cgts.jitterPoint(x, o.jitter);
-				var jy = cgts.jitterPoint(y, o.jitter);
-				var rand = cgts.posNeg();
-				if (rand > 0) {
-					cgts.drawCircle(ctx, jx, jy, cgts.randomRadius(o.minRadius, o.maxRadius));
-				}
-				else {
-					cgts.drawRectangle(ctx, jx, jy, o);
-				}
-				ctx.restore();
-			}
-		}
-		if(o.regenOnWinResize) {
-			window.addEventListener('resize', function(){
-				clearTimeout(cgts.resizeTimeout);
-				cgts.resizeTimeout = setTimeout( function() {
-					cgts.generate(elem, o);
-				}, 250);
-			});
-		}
-	};
-	
+CircleGetsTheSquare.prototype.drawRectangle = function(jx, jy, o) {
+    var ctx = this.context;
+    var rectWidth = Math.floor(Math.random() * (o.maxRectSide - o.minRectSide)) + o.minRectSide;
+    var rectHeight = Math.floor(Math.random() * (o.maxRectSide - o.minRectSide)) + o.minRectSide;
+    var offsetX = -rectWidth/2;
+    var offsetY = -rectHeight/2;
+    var rotationInDegrees = CircleGetsTheSquare.randomAngle();
+    
+    ctx.translate(jx + rectWidth/2, jy + rectHeight/2);
+    ctx.rotate(Math.PI/180 * rotationInDegrees);
+    ctx.translate(offsetX, offsetY);   
+    ctx.fillRect(0, 0, rectWidth, rectHeight);
+        
+};
 
-})(window);
+CircleGetsTheSquare.prototype.generate = function() {
+  var o = this.options;
+  var c = this.canvas;
+  var ctx = this.context;
+  var elem = this.elem;
+  var width = elem.offsetWidth;
+  var height = elem.offsetHeight;
+  if (o.bgColor) elem.style.backgroundColor = o.bgColor;
+  c.width = width;
+  c.height = height;
+  ctx.clearRect(0, 0, width, height); 
+  for (var y = 0; y < height; y = y + o.spacing) {
+    for (var x = 0; x < width; x = x + o.spacing ) {
+      ctx.save();
+      ctx.fillStyle = CircleGetsTheSquare.randomColor(o.rMin, o.rMax, o.gMin, o.gMax, o.bMin, o.bMax, o.aMin, o.aMax);
+      var jx = CircleGetsTheSquare.jitterPoint(x, o.jitter);
+      var jy = CircleGetsTheSquare.jitterPoint(y, o.jitter);
+      var rand = CircleGetsTheSquare.posNeg();
+      if (rand > 0) {
+        this.drawCircle(jx, jy, CircleGetsTheSquare.randomRadius(o.minRadius, o.maxRadius));
+      }
+      else {
+        this.drawRectangle(jx, jy, o);
+      }
+      ctx.restore();
+    }
+  }
+
+};
+
+/*
+ *
+ * static functions and properties
+ *
+ */
+CircleGetsTheSquare.randomColor = function(rMin, rMax, gMin, gMax, bMin, bMax, aMin, aMax) {
+	return 'rgba(' +
+		(Math.floor(Math.random()*rMax) + rMin) +
+		',' +
+		(Math.floor(Math.random()*gMax) + gMin) + 
+		',' + 
+		(Math.floor(Math.random()*bMax) + bMin) + 
+		',' + 
+		Math.floor(Math.random() * ((aMax*10) - (aMin*10)) + (aMin * 10)) / 10 +
+		')';
+};
+
+CircleGetsTheSquare.randomAngle = function() {
+	return Math.floor(Math.random()*360);
+};
+
+CircleGetsTheSquare.randomRadius = function(minRadius, maxRadius){
+	return (Math.floor(Math.random()*minRadius) + maxRadius);
+};
+
+CircleGetsTheSquare.jitterPoint =	function(point, jitter){
+	var jittered = point + CircleGetsTheSquare.posNeg(Math.ceil(Math.random()* jitter)-1);
+	return jittered;
+};
+
+CircleGetsTheSquare.posNeg = function(num) {
+	if (num == undefined) num = 1;
+	var gen = (Math.floor(Math.random()*5)) + 1;
+	return ((gen % 2 == 0) ? 1 : -1) * num;
+};
+
+CircleGetsTheSquare.defaultOptions = {
+  regenOnWinResize  : true,
+  canvasClassName : 'cgtsCanvas',
+  bgColor     : 'white',
+  minRadius   : 30,
+  maxRadius   : 48,
+  minRectSide   : 60,
+  maxRectSide   : 150,
+  rMin      : 0,
+  rMax      : 255,
+  gMin      : 0,
+  gMax      : 255,
+  bMin      : 0,
+  bMax      : 255,
+  aMin      : 0.2,
+  aMax      : 0.8,
+  spacing     : 60,
+  jitter      : 8
+};
+
+window.CircleGetsTheSquare = CircleGetsTheSquare;
